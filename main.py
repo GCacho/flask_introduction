a65345c2fca45996ac1a1f610fa253f53feaf77b
Guillermo Cacho <guillermo.cacho.v@gmail.com>
2022-06-13 17:35:49 -0600

    request_and_response

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-from flask import Flask
+from flask import Flask, redirect, request, make_response
 
 app = Flask(__name__)
 
-@app.route('/') #Donde queremos que corra la app
+@app.route('/')
+def index():
+    user_ip = request.remote_addr # Para capturar la ip del usuario.
+    response = make_response(redirect('/hello')) # Nos redirige a /hello
+    response.set_cookie('user_ip', user_ip)
+    return response
+
+@app.route('/hello') # Donde queremos que corra la app
 def hello():
-    return "Hello World Flask!"
+    user_ip = request.cookies.get('user_ip')
+    return "Hello World Flask!, tu IP es {}".format(user_ip)
