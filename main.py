2b17179e1aa62d698726bd72bbc1e79b38de9ec3
Guillermo Cacho <guillermo.cacho.v@gmail.com>
2022-06-14 09:11:08 -0600

    reestructura_de_archivos_flask

--- main.py
+++ main.py
@@ -1,35 +1,61 @@
-#Flask
-from ensurepip import bootstrap
-from multiprocessing import context
-from flask import Flask
-from flask import request
-from flask import redirect, make_response, render_template
+from flask import request, make_response, redirect, render_template, session, url_for, flash
+import unittest
 
-from flask_bootstrap import Bootstrap
+from app import create_app
+from app.forms import LoginForm
 
-app = Flask(__name__)
-bootstrap = Bootstrap(app)
+app = create_app()
 
 
-todos = ['Comprar Café', 'Enviar Solicitud de Compra', 'Entregar video a productor']
+todos = ['Comprar cafe', 'Enviar solicitud de compra', 'Entregar video a productor ']
+
+
+@app.cli.command()
+def test():
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner().run(tests)
+
 
 @app.errorhandler(404)
 def not_found(error):
     return render_template('404.html', error=error)
 
+
+@app.errorhandler(500)
+def server_error(error):
+    return render_template('500.html', error=error)
+
+
 @app.route('/')
 def index():
-    user_ip = request.remote_addr # Para capturar la ip del usuario.
-    response = make_response(redirect('/hello')) # Nos redirige a /hello
-    response.set_cookie('user_ip', user_ip)
+    user_ip = request.remote_addr
+
+    response = make_response(redirect('/hello'))
+    session['user_ip'] = user_ip
+
     return response
 
-@app.route('/hello') # Donde queremos que corra la app
+
+@app.route('/hello', methods=['GET', 'POST'])
 def hello():
-    user_ip = request.cookies.get('user_ip')
+    user_ip = session.get('user_ip')
+    login_form = LoginForm()
+    username = session.get('username')
+
     context = {
         'user_ip': user_ip,
-        'todos': todos
+        'todos': todos,
+        'login_form': login_form,
+        'username': username
     }
 
-    return render_template('hello.html', **context) # **context es igual a context=context / amplia el diccionario
+    if login_form.validate_on_submit():
+        username = login_form.username.data
+        session['username'] = username
+
+        flash('Nombre de usario registrado con éxito!')
+
+        return redirect(url_for('index'))
+
+    return render_template('hello.html', **context)
+
