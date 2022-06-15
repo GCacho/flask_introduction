5bc96b728d872b7db64f93b486b738345892807a
Guillermo Cacho <guillermo.cacho.v@gmail.com>
2022-06-14 18:37:58 -0600

    Hasta donde llegue / conexion a la base de datos en log

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,8 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
-from flask_login import LoginManager
+
 from .config import Config
 from .auth import auth
-from .models import UserModel
-
-
-login_manger = LoginManager()
-login_manger.login_view = 'auth.login'
-
-
-@login_manger.user_loader
-def load_user(username):
-    return UserModel.query(username)
 
 
 def create_app():
@@ -20,8 +10,6 @@ def create_app():
     bootstrap = Bootstrap(app)
 
     app.config.from_object(Config)
-
-    login_manger.init_app(app)
 
     app.register_blueprint(auth)
 
